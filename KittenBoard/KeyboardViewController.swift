dd6ab01c0d4f69d3e68debfc8481ffdf85662983
Adrien Maranville <Adrien.Maranville@gmail.com>
2017-07-05 17:26:56 -0600

    initial commit with basic functionality.

---
+++ KittenBoard/KeyboardViewController.swift
@@ -0,0 +1,72 @@
+//
+//  KeyboardViewController.swift
+//  KittenBoard
+//
+//  Created by Adrien Maranville on 7/5/17.
+//  Copyright © 2017 Adrien Maranville. All rights reserved.
+//
+
+import UIKit
+
+class KeyboardViewController: UIInputViewController {
+
+    @IBOutlet var nextKeyboardButton: UIButton!
+    
+    override func updateViewConstraints() {
+        super.updateViewConstraints()
+        
+        // Add custom view sizing constraints here
+    }
+    
+    func didPressButton() {
+        let proxy = textDocumentProxy as UITextDocumentProxy
+        proxy.insertText("meow")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let btnKitten = UIButton(type: .system)
+        btnKitten.frame = CGRect(x: 150, y: 40, width: 100, height: 120)
+        btnKitten.setBackgroundImage(UIImage(named: "kitten.png"), for: .normal)
+        btnKitten.addTarget(self, action: #selector(KeyboardViewController.didPressButton), for: .touchUpInside)
+        self.view.addSubview(btnKitten)
+        
+        // Perform custom UI setup here
+        self.nextKeyboardButton = UIButton(type: .system)
+        
+        self.nextKeyboardButton.setTitle(NSLocalizedString("Next Keyboard", comment: "Title for 'Next Keyboard' button"), for: [])
+        self.nextKeyboardButton.sizeToFit()
+        self.nextKeyboardButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.nextKeyboardButton.addTarget(self, action: #selector(handleInputModeList(from:with:)), for: .allTouchEvents)
+        
+        self.view.addSubview(self.nextKeyboardButton)
+        
+        self.nextKeyboardButton.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
+        self.nextKeyboardButton.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated
+    }
+    
+    override func textWillChange(_ textInput: UITextInput?) {
+        // The app is about to change the document's contents. Perform any preparation here.
+    }
+    
+    override func textDidChange(_ textInput: UITextInput?) {
+        // The app has just changed the document's contents, the document context has been updated.
+        
+        var textColor: UIColor
+        let proxy = self.textDocumentProxy
+        if proxy.keyboardAppearance == UIKeyboardAppearance.dark {
+            textColor = UIColor.white
+        } else {
+            textColor = UIColor.black
+        }
+        self.nextKeyboardButton.setTitleColor(textColor, for: [])
+    }
+
+}
